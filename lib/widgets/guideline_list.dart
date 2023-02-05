b276f938ea4408e389f8a0118394a760e5a5bc03
mohammed <m.199671.ms@gmail.com>
2023-02-05 18:51:37 +0300

    last updata

--- lib/widgets/guideline_list.dart
+++ lib/widgets/guideline_list.dart
@@ -1,6 +1,4 @@
-import 'package:dr_seta/helpers/constants.dart';
 import 'package:dr_seta/models/surgery_antibiotics_model.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class GuidelineList extends StatelessWidget {
@@ -11,94 +9,181 @@ class GuidelineList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(8.0),
+      padding: const EdgeInsets.all(10.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Align(
-              alignment: Alignment.centerLeft,
-              child: Text(
-                data.procedure,
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-              )),
-          SizedBox(
+          Text(
+            data.procedure,
+            style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(
             height: 5,
           ),
-          Align(
-              alignment: Alignment.centerLeft,
-              child: RichText(text: TextSpan(text: "Duration: ",style: TextStyle(color: Colors.grey[700]), children: [
-                TextSpan(
-                    text: data.prophylaxisDuration,style: TextStyle(color: Colors.black))
-
-              ]),
-
-
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                "Duration: ",
+                style: TextStyle(
+                    color: Colors.grey[700], fontWeight: FontWeight.bold),
               ),
-            ),
-          SizedBox(
+              Text(data.prophylaxisDuration,
+                  style: const TextStyle(color: Colors.black)),
+            ],
+          ),
+          const SizedBox(
             height: 5,
           ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text("List of Drugs"),
-                Text(
-                    "${data.time.toDate().day}/${data.time.toDate().month}/${data.time.toDate().year}")
-              ],
-            ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Text(
+                "List of Drugs",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                  "${data.time.toDate().day}/${data.time.toDate().month}/${data.time.toDate().year}")
+            ],
           ),
           ListView.builder(
-              physics: ScrollPhysics(),
+              physics: const ScrollPhysics(),
               itemCount: data.dosage.length,
               shrinkWrap: true,
               itemBuilder: (context, index) {
                 return Card(
                   child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 0,vertical: 5),
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 0, vertical: 5),
                     child: ListTile(
-                        leading: Text(data.dosage[index].line.toString()),
+                        leading: Text(
+                          data.dosage[index].line.toString(),
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
                         title: Text(
-                            "${data.dosage[index].drug} for ${data.dosage[index].classification}"),
-                        subtitle: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-
-
-
-                                  RichText(text: TextSpan(text: "First time administration ",style: TextStyle(color: Colors.grey[700]), children: [
-                                    TextSpan(
-                                        text: "within ${data.dosage[index].timeOfFirstDoseInMinutes} mins",style: TextStyle(color: Colors.black))
-
-                                  ]),
-
-
-                            ),
-                            if (data.dosage[index].firstDosageComment.isNotEmpty)
-                              RichText(text: TextSpan(text: "Note: ",style: TextStyle(color: Colors.grey[700]), children: [
-                                TextSpan(
-                                    text: " ${data.dosage[index].firstDosageComment}",style: TextStyle(color: Colors.black))
-
-                              ])),
-                            if (data.dosage[index].timeOfRedosingInMinutes > 0.0 && data.dosage[index].timeOfRedosingInMinutes !=null) RichText(text: TextSpan(text: "Time interval for doses: ",style: TextStyle(color: Colors.grey[700]), children: [
-                              TextSpan(
-                                  text: " every ${data.dosage[index].timeOfRedosingInMinutes / 60.toInt()} hrs",style: TextStyle(color: Colors.black))
-
-                            ])),
-                            if (data.dosage[index].comment.isNotEmpty)
-                              RichText(text: TextSpan(text:  "Note: ",style: TextStyle(color: Colors.grey[700]), children: [
-                                TextSpan(
-                                    text: "${data.dosage[index].comment}",style: TextStyle(color: Colors.black))
-
-                              ])),
-
-                          ],
+                          "${data.dosage[index].drug} for ${data.dosage[index].classification}",
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        subtitle: Padding(
+                          padding: const EdgeInsets.all(3.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const SizedBox(
+                                height: 10,
+                              ),
+                              RichText(
+                                text: TextSpan(
+                                    text: "First time administration: ",
+                                    style: TextStyle(
+                                      color: Colors.grey[700],
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                    children: [
+                                      TextSpan(
+                                          text:
+                                              "within ${data.dosage[index].timeOfFirstDoseInMinutes} mins",
+                                          style: const TextStyle(
+                                              fontSize: 14,
+                                              color: Colors.black))
+                                    ]),
+                              ),
+                              if (data
+                                  .dosage[index].firstDosageComment.isNotEmpty)
+                                Column(
+                                  children: [
+                                    const Divider(
+                                      color: Colors.black,
+                                    ),
+                                    RichText(
+                                        text: TextSpan(
+                                            text: "Note: ",
+                                            style: TextStyle(
+                                                color: Colors.grey[700],
+                                                fontSize: 16,
+                                                fontWeight: FontWeight.bold),
+                                            children: [
+                                          TextSpan(
+                                              text:
+                                                  " ${data.dosage[index].firstDosageComment}",
+                                              style: const TextStyle(
+                                                  fontSize: 14,
+                                                  color: Colors.black))
+                                        ])),
+                                  ],
+                                ),
+                              if (data.dosage[index].timeOfRedosingInMinutes >
+                                      0.0 &&
+                                  data.dosage[index].timeOfRedosingInMinutes !=
+                                      null)
+                                Column(
+                                  children: [
+                                    const Divider(
+                                      color: Colors.black,
+                                    ),
+                                    RichText(
+                                      text: TextSpan(
+                                        text: "Time interval for doses: ",
+                                        style: TextStyle(
+                                            color: Colors.grey[700],
+                                            fontSize: 16,
+                                            fontWeight: FontWeight.bold),
+                                        children: [
+                                          TextSpan(
+                                              text:
+                                                  " every ${data.dosage[index].timeOfRedosingInMinutes / 60.toInt()} hrs",
+                                              style: const TextStyle(
+                                                  fontSize: 14,
+                                                  color: Colors.black))
+                                        ],
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              if (data.dosage[index].comment.isNotEmpty)
+                                Column(
+                                  children: [
+                                    const Divider(
+                                      color: Colors.black,
+                                    ),
+                                    RichText(
+                                      text: TextSpan(
+                                        text: "Note: ",
+                                        style: TextStyle(
+                                            color: Colors.grey[700],
+                                            fontSize: 16,
+                                            fontWeight: FontWeight.bold),
+                                        children: [
+                                          TextSpan(
+                                              text: data.dosage[index].comment,
+                                              style: const TextStyle(
+                                                  fontSize: 14,
+                                                  color: Colors.black))
+                                        ],
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                            ],
+                          ),
                         ),
                         trailing: Column(
                           children: [
-                           for(int i=0; i<data.dosage[index].amount.split("=").length;i++) Text(
-                                "${data.dosage[index].amount.split("=")[i]} ${data.dosage[index].suffix.split("=")[i]}",style: TextStyle(fontWeight: FontWeight.bold),),
+                            for (int i = 0;
+                                i < data.dosage[index].amount.split("=").length;
+                                i++)
+                              Text(
+                                "${data.dosage[index].amount.split("=")[i]} ${data.dosage[index].suffix.split("=")[i]}",
+                                style: const TextStyle(
+                                    fontWeight: FontWeight.bold),
+                              ),
                             Text(data.dosage[index].route),
                           ],
                         )),
